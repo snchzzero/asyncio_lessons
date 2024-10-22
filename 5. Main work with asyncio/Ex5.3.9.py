5f7323a9b55a717aa9db03a35ac8341812dd0b11
snchzzero <aaevstafev@gmail.com>
2024-10-22 13:14:04 +0300

    done - ex5.3.9
    Signed-off-by: Александр Евстафьев <evstafev@algont.ru>

---
+++ 5. Main work with asyncio/Ex5.3.9.py
@@ -0,0 +1,25 @@
+import asyncio
+
+async def coroutine_1():
+    print("Первое сообщение от корутины 1")
+    await asyncio.sleep(0.3)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 1")
+
+async def coroutine_2():
+    print("Первое сообщение от корутины 2")
+    await asyncio.sleep(0.2)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 2")
+
+async def coroutine_3():
+    print("Первое сообщение от корутины 3")
+    await asyncio.sleep(0.1)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 3")
+
+async def main():
+    await asyncio.gather(
+        coroutine_1(),
+        coroutine_2(),
+        coroutine_3(),
+    )
+
+asyncio.run(main())
